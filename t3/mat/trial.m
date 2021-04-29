0dd5803a086be48b17e3fb22457cbd36b6fd0a63
BeatrizRosalino <beatriz.rosalino@tecnico.ulisboa.pt>
2021-04-29 15:52:50 +0100

    updated t3 octave

--- t3/mat/trial.m
+++ t3/mat/trial.m
@@ -5,8 +5,8 @@ vT=25e-3;
 f=1e3;
 w=2*pi*f;
 RON=80
-R=1e3
-C=1e-6
+R=5e3
+C=5e-6
 vON=0.65;
 
 t=linspace(0, 5e-3, 1000);
@@ -14,13 +14,12 @@ A = 5;
 vS=A*cos(w*t);
 vO= zeros(1,length(t));
 for i=1:length(t)
-  if(vS(i) - 2*vON >=0)
-    vO(i) = vS(i) - 2*vON;
+  if(vS(i) - vON >=0)
+    vO(i) = vS(i) - vON;
   else
-    vO(i) = - vS(i) - 2*vON;
+    vO(i) = 0;
   endif 
 endfor
-
 
 figure
 plot(t*1000, vO)
@@ -42,42 +41,42 @@ c= sym (C);
 r= sym (R);
 W= sym (w);
 syms to;
-n=C*A*R*w*sin(w*to)==-A*cos(w*to)-2*vON;
+n=C*A*R*w*sin(w*to)==A*cos(w*to);
 tof=solve(n);
   for i=1:2
 	  toff=tof(i);
   if toff>=0
-    tOFF=double(toff);
+    tOFF=double(toff)
 endif
 endfor
 	 
-vOnexp = A*cos(w*tOFF)*exp(-(t-tOFF)/R/C/2)+2*vON;
+vOnexp = A*cos(w*tOFF)*exp(-(t-tOFF)/R/C);
 
 figure
 for i=1:length(t)
-if (vS(i)-2*vON >= 0)
-    vOhr(i) = vS(i) - 2*vON;
+if (vS(i)-vON >= 0)
+    vOhr(i) = vS(i) - vON;
   else
-    vOhr(i) = -vS(i) - 2*vON;
+    vOhr(i) = 0;
   endif
 endfor
 
   for i=1:length(t)
-  if(vS(i) - 2*vON >=0)
-    vO(i) = vS(i) - 2*vON;
+  if(vS(i) - vON >=0)
+    vO(i) = vS(i) - vON;
   else
-    vO(i) = - vS(i) - 2*vON;
+    vO(i) = 0;
   endif 
 endfor
   
 plot(t*1000, vOhr)
 hold
-
+T=1/f
 for i=1:length(t)
-  if t(i) < tOFF
-    vO(i) = vOhr(i);
-  elseif vOnexp(i) > vOhr(i)
-   vO(i) = vOnexp(i);
+if 0<t(i) < tOFF
+	vO(i) = vOhr(i);
+elseif vOnexp(i)-vON> vOhr(i)
+   vO(i) = vOnexp(i)-vON;
   else 
     vO(i) = vOhr(i);
   endif
