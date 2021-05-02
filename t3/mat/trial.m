0bc93ad18b0f57a094c9b3bfafbde58107c4e427
sofiaguerreiro <sofia.guerreiro@tecnico.ulisboa.pt>
2021-05-02 20:38:09 +0100

    updated octave

--- t3/mat/trial.m
+++ t3/mat/trial.m
@@ -10,55 +10,40 @@ vON=0.65;
 
 t=linspace(0, 0.2, 1000);
 A = 13.8;
-vS=A*sin(w*t);
-vO= zeros(1,length(t));
+vS=A*cos(w*t);
+vOhr= zeros(1,length(t));
 for i=1:length(t)
-  if(vS(i) - 2*vON >=0)
-    vO(i) = vS(i) - 2*vON;
+  if(vS(i) >= 2*vON)
+    vOhr(i) = vS(i) - 2*vON;
+  elseif (vS(i)>0 || (abs(vS(i))<2*vON && vS(i)<0)) %%%%%%%%%%%FALTA ARRANJAR
+    vOhr(i) = -vS(i) + 2*vON;
   else
-    vO(i) = -vS(i) - 2*vON;
+    vOhr(i) = -vS(i) - 2*vON;
   endif 
 endfor
 
 figure
-plot(t*1000, vO)
+plot(t*1000, vOhr)
 title("Output voltage v_o(t)")
 xlabel ("t[ms]")
 ylabel ("v_o[V]")
 print ("vo.eps", "-depsc");
 
-%{envelope detector
-vOhr = zeros(1, length(t));
-tOFF=1
-%/w*atan(-1/(w*R*C))
+%envelope detector
+tOFF=atan(1/(w*R*C))/w
 	 
-vOnexp = A*cos(w*tOFF)*exp(-(t-tOFF)/R/C);
+vOnexp = (A*cos(w*tOFF)- 2*vON)*exp(-(t-tOFF)/(R*C));
 
 figure
-for i=1:length(t)
-if (vS(i)-vON >= 0)
-    vOhr(i) = vS(i) - vON;
-  else
-    vOhr(i) = 0;
-  endif
-endfor
-
-  for i=1:length(t)
-  if(vS(i) - vON >=0)
-    vO(i) = vS(i) - vON;
-  else
-    vO(i) = 0;
-  endif 
-endfor
 
 plot(t*1000, vOhr)
 hold
 T=1/f
 for i=1:length(t)
-if 0<t(i) < tOFF
+if t(i) < tOFF
 	vO(i) = vOhr(i);
-elseif vOnexp(i)-vON> vOhr(i)
-   vO(i) = vOnexp(i)-vON;
+  elseif vOnexp(i) > vOhr(i)
+   vO(i) = vOnexp(i);
   else 
     vO(i) = vOhr(i);
   endif
@@ -72,18 +57,10 @@ print ("venvlope.eps", "-depsc");
 
 
 
-
-
-
-
 %lpf response
-t=linspace(0, 50e-3, 1000);
-Is=1e-9;
-VON=0.65
-vlim =3*VON
-  rd=2*vT/(Is*exp(3*vON/2/vT));
-f=400;
-w=2*pi*f;
+Is=1e-12;
+vlim =3*vON
+rd=2*vT/(Is*exp(3*vON/2/vT));
 vo =3*rd/(R+3*rd) *A* cos(w*t);
 figure;
 title("Output voltage v_o(t)")
@@ -97,8 +74,7 @@ for i=1:length(t)
     vO(i) = -vlim;
   endif
 endfor
-  VO=3*VON;
+  VO=3*vON;
 vO=VO+vo;
 plot(t*1000, vo);
 print ("vrec.eps", "-depsc");
- %}
