fd7c7364f83f2899864eda61b9f355e7a9220da4
BeatrizRosalino <beatriz.rosalino@tecnico.ulisboa.pt>
2021-06-05 20:22:36 +0100

    updated octave

--- t5/mat/T5_ni.m
+++ t5/mat/T5_ni.m
@@ -4,51 +4,66 @@ pkg load symbolic;
 
 VIN=exp(j*pi/2);
 R1=1000;
-ZR1=1/(2/R1);
 R2=1000;
-C1=3*1E-6+2*0.22E-6;
+ZR2=1/(2/1000);
+C1=0.22E-6;
 C2=0.22E-6;
 R3=1/(3/10000);
 R4=100000*3;
 
 
-f=1000;
-w=2*pi*f;
-ZC1=1/(j*w*C1);
-ZC2=1/(j*w*C2);
-N=[1,0,0,0,0;
-    -1/ZC1,1/ZR1+1/ZC1, 0, 0, 0;
-    0,0, 1/R3+1/R4, -1/R4,0;
-    0, +1+R4/R3, -1-R4/R3,-1,0;
-    0,0,0,-1/R2, 1/R2+1/ZC2
-   ];
-b=[VIN;0;0;0;0];
-V=N\b;
-Gain=20*log10(abs(V(5)/V(1)))
-  I1=(V(1)-V(2))/ZC1;
-ZIN=(V(1)/I1)
+G=@(f) (1+R4/R3)*1/(1/R1+(j*2*pi*f*C1))*1/((2*pi*j*f*C2)+1/ZR2)/(1/(C1*2*pi*j*f)*ZR2);
+fc=1000;
+Gain=20*log10(abs(G(fc)))
+Phase=arg(G(fc))
+
+  V2=VIN/(2*pi*j*C1)*1/(1/R1+2*pi*j*C1);
+Iin=(V2-VIN)/(2*pi*j*C1);
+ZIN=VIN/Iin
+
+%Iout=1-
+
   
 %--------------------------------------------
 G=zeros(1,70);
+Gain=zeros(1,70);
+Phase=zeros(1,70);
 f=logspace(1,8,70);
 for i=1:70
 w=2*pi*f(i);
 ZC1=1/(j*w*C1);
 ZC2=1/(j*w*C2);
-N=[1,0,0,0,0;
-    -1/ZC1,1/ZR1+1/ZC1, 0, 0, 0;
-    0,0, 1/R3+1/R4, -1/R4,0;
-    0, +1+R4/R3, -1-R4/R3,-1,0;
-    0,0,0,-1/R2, 1/R2+1/ZC2
-   ];
-b=[VIN;0;0;0;0];
-V=N\b;
-  G(i)=20*log10((V(5)/V(1)));
+G(i)=(1+R4/R3)*1/(1/R1+1/ZC1)*1/(1/ZC2+1/ZR2)/(ZC1*ZR2);
+Gain(i)=20*log10(abs(G(i)));
+Phase(i)=angle(G(i));
 endfor
 
+MaxGain=max((Gain))
+Cutoff=MaxGain-3
+ 
+flowcutoff=1/(2*pi*C1*R1)
+fhighcutoff=1/(2*pi*C2*ZR2)
+centralf=sqrt(flowcutoff*fhighcutoff)
 
+  
+H=@(f) 20*log10(abs((1+R4/R3)*1/(1/R1+(j*2*pi*f*C1))*1/((2*pi*j*f*C2)+1/ZR2)/(1/(C1*2*pi*j*f)*ZR2)))-32.659;
+S=[10, 1000];
+flow=fzero(H,S)
+M=[1000, 10000];
+fhigh=fzero(H,M)
+centralf2=sqrt(flow*fhigh)
+Max=H(centralf)+32.659
+  
 fig=figure();
-plot(log10(f),G);
+plot(log10(f),Gain);
 xlabel ("Frequency[Hz]")
 ylabel ("vo/vi[dB]")
 print(fig, "gain.eps", "-depsc");
+
+
+
+figg=figure();
+plot(log10(f),Phase);
+xlabel ("Frequency[Hz]")
+ylabel ("Phase[rad]")
+print(figg, "phase.eps", "-depsc");
